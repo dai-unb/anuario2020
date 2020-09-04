8cc6d0a85b1179f558c40eae3bdbb678c7e4b6a4
geisarnovais <68559159+geisarnovais@users.noreply.github.com>
2020-09-04 09:27:26 -0300

    Atualização Dados Gerais
    Atualiza tabela de dados gerais e script gera dados gerais.

--- scripts/03_gera_tabela_geral.R
+++ scripts/03_gera_tabela_geral.R
@@ -11,6 +11,120 @@ load("dados_identificados/Completo14042020.RData")
 
 anobase <- 2019
 
+### define o ano do CENSO
+
+sem1 <- 12019
+sem2 <- 22019
+
+# Componentes institucionais ----
+comp_inst <- rio::import("dados_gerais/dados_gerais_tabelas.xlsx", sheet=1)
+
+# Números da UnB ----
+
+vagas_oferecidas <- rio::import("dados_gerais/dados_gerais_tabelas.xlsx", sheet=2)
+
+inscritos <- rio::import("dados_gerais/dados_gerais_tabelas.xlsx", sheet=3)
+
+demanda <- rio::import("dados_gerais/dados_gerais_tabelas.xlsx", sheet=4)
+
+
+### Ingressantes
+
+Arquivo42$forma_ingresso <- ifelse(Arquivo42$Ing_Enem==1,"Sisu/Enem",
+                                     ifelse(Arquivo42$Ing_Pas==1,"PAS",
+                                            ifelse(Arquivo42$Ing_Vest==1, "Vestibular",
+                                                   ifelse(Arquivo42$Ing_Trans==1|
+                                                            Arquivo42$Ing_Rem==1|
+                                                            Arquivo42$Ing_PECG==1|
+                                                            Arquivo42$Ing_Esp==1,"Por outras vias",0))))
+  
+  ingressantes <- Arquivo42 %>% 
+    filter(Sem_Ingresso==sem1 | Sem_Ingresso==sem2) %>% 
+    group_by(forma_ingresso,Sem_Ref) %>% 
+    tally() %>% 
+    spread(Sem_Ref,n)
+
+  names(ingressantes)[1:3] <- c("INGRESSANTES", "1º Sem.","2º Sem.")   
+  
+  ingressantes <- ingressantes %>%
+   filter(INGRESSANTES!=0)
+
+ # adcionando uma linha de total 
+ ingressantes <- adorn_totals(ingressantes, where = "row", fill = "-", na.rm = TRUE, name = "Total")
+ 
+ # exportando a tabela
+ rio::export(ingressantes, "dados_gerais/Tabela_ingressantes.xlsx")  
+
+### Cursos 
+ 
+### Diplomados  
+ 
+ # graduação
+ 
+ formado_grad <- Arquivo42
+ formado_grad$grad <- "Graduação"
+ formado_grad <- formado_grad %>% 
+   filter(Vinculo=="Formado") %>% 
+   group_by(Sem_Ref, grad) %>% 
+   tally() %>% 
+   spread(Sem_Ref,n)
+ 
+ names(formado_grad)[1:3] <- c("DIPLOMADOS", "1º Sem.","2º Sem.")   
+ 
+ # pós
+ 
+ Mest <- Completo %>% 
+   filter(`Ano Ingresso Opcao`<= anobase,
+          `Ano Saida Opcao`>= anobase,
+          `Nivel Curso`=="Mestrado",
+          `Forma Saida` != "Anulacao de Registro")
+ 
+ Mest$mest <-"Mestrado"
+ 
+ 
+ formado_mest <- Mest %>% 
+   filter(`Ano Saida Opcao`==anobase,
+          (`Semestre Saida Opcao`==2|`Semestre Saida Opcao`==1),
+          str_detect(`For. Saida Opcao`, "Formatura")) %>%
+   group_by(mest,`Semestre Saida Opcao`) %>% 
+   tally() %>% 
+   spread(`Semestre Saida Opcao`,n)
+  
+ names(formado_mest)[1:3] <- c("DIPLOMADOS", "1º Sem.","2º Sem.") 
+
+ Doc <- Completo %>% 
+   filter(`Ano Ingresso Opcao` <= anobase,
+          `Ano Saida Opcao` >= anobase,
+          `Nivel` == "Doutorado",
+          `Forma Saida` != "Anulacao de Registro")
+ 
+ Doc$doc <-"Doutorado"
+ 
+ formado_doc <- Doc %>% 
+   filter(`Ano Saida Opcao`==anobase,
+          (`Semestre Saida Opcao`==2|`Semestre Saida Opcao`==1),
+          str_detect(`For. Saida Opcao`, "Formatura")) %>%
+   group_by(doc,`Semestre Saida Opcao`) %>% 
+   tally() %>% 
+   spread(`Semestre Saida Opcao`,n)
+ 
+ names(formado_doc)[1:3] <- c("DIPLOMADOS", "1º Sem.","2º Sem.") 
+ 
+ 
+ formados <- rbind(formado_grad,formado_mest,formado_doc)
+ rm(formado_grad,formado_mest,formado_doc)
+ 
+ formados <- adorn_totals(formados, where = "row", fill = "-", na.rm = TRUE, name = "Total")
+ 
+ 
+ 
+ escolaridade_docente <- rio::import("dados_gerais/dados_gerais_tabelas.xlsx", sheet=5)
+ escolaridade_tecnico <- rio::import("dados_gerais/dados_gerais_tabelas.xlsx", sheet=6)
+ area_fisica <- rio::import("dados_gerais/dados_gerais_tabelas.xlsx", sheet=7)
+ bce <- rio::import("dados_gerais/dados_gerais_tabelas.xlsx", sheet=8)
+ edu <- rio::import("dados_gerais/dados_gerais_tabelas.xlsx", sheet=9)
+
+ 
 # Graduação ---------------------------------------------------------------
 
 Grad_Reg <- Arquivo42 %>% 
