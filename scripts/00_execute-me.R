aa730bcfa55483d45d82efa829a80a5fc7efdf45
Roberto de Freitas Neder <robertofreitasneder88@gmail.com>
2020-11-22 02:12:36 -0300

    Gráficos e Revisão

--- scripts/00_execute-me.R
+++ scripts/00_execute-me.R
@@ -1,13 +1,13 @@
 ### script para atualizar todas as tabelas
 ### e para ativar o RENV (gerenciamento de pacotes)
 ### vai ser construído aos poucos
-options(encoding = "UTF-8", 
-        OutDec = ",", 
-        scipen = 9)
+#options(encoding = "UTF-8", 
+        #OutDec = ",", 
+        #scipen = 9)
 
-source("scripts/01_renv_setup.R")
-source("scripts/02_pega_dados_censo.R")
-source("scripts/03_gera_tabela_geral.R")
+#source("scripts/01_renv_setup.R")
+#source("scripts/02_pega_dados_censo.R")
+#source("scripts/03_gera_tabela_geral.R")
 # source("scripts/04_gera_tabelas_graduacao.R")  - ARRUMAR ANTES
 # source("scripts/05_gera_tabelas_mestrado.R")   - ARRUMAR ANTES
 # source("scripts/06_gera_tabelas_doutorado.R")  - ARRUMAR ANTES
@@ -15,5 +15,6 @@ source("scripts/03_gera_tabela_geral.R")
 
 ### compila o livro
 ### vou deixar comentado para só ser usado quando necessário
-# bookdown::render_book("anuario2020.Rmd", encoding = "UTF-8")
-# rstudioapi::viewer("_book/index.html")
+
+bookdown::render_book("anuario2020.Rmd", encoding = "UTF-8")
+rstudioapi::viewer("_book/index.html")
